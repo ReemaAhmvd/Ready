b3230e51bb8009f7e6328a3575c31abc4a52a353
ReemaAhmvd <ReemaAhmvd@outlook.com>
2023-12-21 16:33:18 +0300

    Initial Commit

---
+++ NC2/NC2App.swift
@@ -0,0 +1,17 @@
+//
+//  NC2App.swift
+//  NC2
+//
+//  Created by Reema Alshalan on 08/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct NC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
